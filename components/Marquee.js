214f3b24dd5d6a308af753d4cf68a51c9370d225
Silver Lee-A-Fong <silverlee2000@gmail.com>
2022-04-19 00:23:52 +0200

    Change marquee

--- components/Marquee.js
+++ components/Marquee.js
@@ -1,22 +1,22 @@
 function Marquee() {
   return (
-    <div class="relative bg-gray-500 flex overflow-x-hidden">
-      <div class="py-4 animate-marquee whitespace-nowrap">
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+    <div className="relative bg-gray-500 flex overflow-x-hidden">
+      <div className="py-4 animate-marquee whitespace-nowrap">
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
       </div>
 
-      <div class="absolute top-0 py-4 animate-marquee2 whitespace-nowrap">
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
-        <span class="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+      <div className="absolute top-0 py-4 animate-marquee2 whitespace-nowrap">
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
+        <span className="text-3xl mx-4 text-white font-akira">Dripping Dinos</span>
       </div>
     </div>
   );
