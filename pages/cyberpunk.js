07b8d715423ab5b0ecd274ae9bdfe842b0ec7eb2
Silver Lee-A-Fong <silverlee2000@gmail.com>
2022-05-01 20:44:58 +0200

    roadmap

--- pages/cyberpunk.js
+++ pages/cyberpunk.js
@@ -172,7 +172,7 @@ const teamData = [
       <Marquee />
 
       <Footer />
-      {/* <Roadmap buttonRef={roadmapRef} changeColor={changeColor} /> */}
+      <Roadmap buttonRef={roadmapRef} changeColor={changeColor} />
     </div>
   );
 }
